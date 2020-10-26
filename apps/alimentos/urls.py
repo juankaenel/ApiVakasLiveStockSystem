6e661c870a5b1eaedba56c9b28259b8e17dcd1c3
Juan Kaenel <kaenel10@gmail.com>
2020-10-26 19:12:44 -0300

    Actualizando alimentos

--- apps/alimentos/urls.py
+++ apps/alimentos/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.apiOverView,name=""),
-    path('alimentos/',views.alimento_is,name="alimento_is"),
-    path('alimentos/<str:pk>',views.alimento_sud,name="alimento_sud"),
+    path('alimentos/',views.AlimentosList.as_view(),name="alimentos_list"),
+    path('alimentos/<str:pk>',views.AlimentosDetail.as_view(),name="alimentos_detail"),
 ]
 
--- apps/alimentos/views.py
+++ apps/alimentos/views.py
@@ -1,48 +1,57 @@
-from rest_framework import viewsets
+# django rest
+from django.http import Http404
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
+from rest_framework.views import APIView
+# apps
 from .models import Alimentos
 from .serialize import AlimentosSerializer
 
 
-@api_view(['GET'])
-def apiOverView(request):
-    api_urls = {
-        'index' : '/alimentos', #Get
-        'store' : '/alimentos', #Post
-        'show' : '/alimentos/<str:pk>', #Get
-        'update': '/alimentos/<str:pk>', #Put,
-        'destroy': '/alimentos/<str:pk>', #Delete
-    }
-    return Response(api_urls)
-
 # END POINTS
-@api_view(['GET','POST'])
-def alimento_is(request): #is -> index, store
-   if request.method == 'GET':
-       alimento = Alimentos.objects.all()
-       serializer = AlimentosSerializer(alimento,many=True)
-       return Response(serializer.data)
-   elif request.method == 'POST':
-       serializer = AlimentosSerializer(data=request.data)
-       if serializer.is_valid():
-           serializer.save()
-       return Response(serializer.data)
+class AlimentosList(APIView):
+    """
+    Lista todas los alimentos, o crea uno nuevo.
+    """
 
-@api_view(['GET','DELETE','PUT'])
-def alimento_sud(request,pk): #sud -> show, update, delete
-   if request.method == 'GET':
-       alimento = Alimentos.objects.get(id=pk)
-       serializer = AlimentosSerializer(alimento, many=False)
-       return Response(serializer.data)
-   elif request.method == 'PUT':
-       alimento = Alimentos.objects.get(id=pk)
-       serializer = AlimentosSerializer(instance=alimento,data=request.data)
-       if serializer.is_valid():
-           serializer.save()
-       return Response(serializer.data)
-   elif request.method == 'DELETE':
-       alimento = Alimentos.objects.get(id=pk)
-       alimento.delete()
-       return Response('Alimento borrado con éxito!')
+    def get(self, request, format=None):
+        alimentos = Alimentos.objects.all()
+        serializer = AlimentosSerializer(alimentos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = AlimentosSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AlimentosDetail(APIView):
+    """
+    Recupera, actualiza o elimina una alimentación.
+    """
+
+    def get_object(self, pk):
+        try:
+            return Alimentos.objects.get(pk=pk)
+        except Alimentos.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        alimento = self.get_object(pk)
+        serializer = AlimentosSerializer(alimento)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        alimento = self.get_object(pk)
+        serializer = AlimentosSerializer(alimento, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        alimento = self.get_object(pk)
+        alimento.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
